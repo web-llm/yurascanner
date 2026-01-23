85dc94d580792efe48d0adde286f1902e710e743
Mimi <1119186082@qq.com>
2026-01-23 17:10:07 +0800

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+node src/main.js https://metaso.cn \
+  --username admin \
+  --password password \
+  --model gpt-5-mini \
+  --model-endpoint https://tianshu.tones-ai.com/v1/ \
+  --autotask \
+  --screenshot \
+  -t 60
